67bd1099a9f4ca7107d103ba6ef2f3787827aa3b
YaroslavKormushyn <kormushyn.slavik@gmail.com>
2017-04-06 17:26:07 +0300

    Add project files.

---
+++ LvivAdviser.Domain/Abstract/ContentRepository.cs
@@ -0,0 +1,23 @@
+﻿using LvivAdviser.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LvivAdviser.Domain.Abstract.Interfaces;
+
+namespace LvivAdviser.Domain.Abstract
+{
+	class ContentRepository : Repository<Content>, IContentRepository
+	{
+		public ContentRepository(DbContext context) : base(context)
+		{
+		}
+
+		public AppDbContext AppDbContext
+		{
+			get { return Context as AppDbContext; }
+		}
+	}
+}
---
+++ LvivAdviser.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LvivAdviser.WebUI.MvcApplication" Language="C#" %>
