845107f4b4601ac3e535b1053766321625f0a4ed
UlyanaAndrukhiv <ullann1ylaa@gmail.com>
2017-04-27 22:12:58 +0300

    Update IRepository.cs

--- LvivAdviser.Domain/Abstract/Interfaces/IRepository.cs
+++ LvivAdviser.Domain/Abstract/Interfaces/IRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using LvivAdviser.Domain.Entities;
 
 namespace LvivAdviser.Domain.Abstract.Interfaces
 {
